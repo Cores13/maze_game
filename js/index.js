b0f4b59e97ad2d3aba7f7b81718b9d7ad73f5794
Amir Kasumovic <amir@ideaology.io>
2021-06-10 15:46:11 +0200

    Difficulty fix

--- js/index.js
+++ js/index.js
@@ -503,7 +503,7 @@ var sprite;
 var finishSprite;
 var maze, draw, player;
 var cellSize;
-var difficulty = 10;
+var difficulty = 25;
 // sprite.src = 'media/sprite.png';
 
 window.onload = function() {
